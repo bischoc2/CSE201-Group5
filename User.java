acd44d3b90ac9ffdafcd52ee97c70dab6dead9c4
jbonsu13 <72279494+jbonsu13@users.noreply.github.com>
2020-10-19 17:02:22 -0400

    Updated User.java
    Made compatible with ImageLibrary.
    Added getImageLibrary Method and an associated instance variable. Edited the constructer to reflect this change.

--- User.java
+++ User.java
@@ -1,16 +1,21 @@
-
 public class User {
 	private String userName;
 	private String password;
+	private ImageLibrary imagelibrary;
 	
 	public User(String u, String p) {
 		// TODO Auto-generated constructor stub
 		userName = u;
 		password = p;
+		imagelibrary = new ImageLibrary();
 	}
 	public boolean login(String user, String pass) {
 		if(user.equals(this.userName) && pass.equals(this.password)) return true;
 		return false;
+	}
+	
+	public ImageLibrary getImageLibrary() {
+		return imagelibrary;
 	}
 
 }
