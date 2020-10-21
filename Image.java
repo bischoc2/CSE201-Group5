b6b2f1e37be453c397b14776bddff6f97e11d0d0
Leo Su <50029233+Bpleo@users.noreply.github.com>
2020-10-21 18:36:30 -0400

    Add saturation and mirror method

--- Image.java
+++ Image.java
@@ -1,7 +1,14 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 import java.awt.image.*;
 import java.io.*;
 
 import javax.imageio.*;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.CvType;
 
 public class Image {
 	private BufferedImage picture;
@@ -32,9 +39,11 @@ public class Image {
 	 * restore all the changes user made to the image
 	 * 
 	 * @return true if restore successfully, else otherwise
+	 * @throws IOException 
 	 */
-	public boolean restore() {
-		return false;
+	public boolean restore() throws IOException {
+		picture = ImageIO.read(new File(ipath));
+		return true;
 	}
 
 	/**
@@ -52,8 +61,28 @@ public class Image {
 	 * 
 	 * @param degree the number of degree that user want to change to
 	 */
-	public void saturation(int degree) {
-
+	public void saturation(double degree) {
+		int width = picture.getWidth();
+		int height = picture.getHeight();
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				Color c = new Color(picture.getRGB(j, i));
+				int r = (int)(c.getRed());
+				int g = (int)(c.getGreen());
+				int b = (int)(c.getBlue());
+				float[] hsb = Color.RGBtoHSB(r, g, b, null);
+				float hue = hsb[0];
+				float saturation = hsb[1];
+				float brightness = hsb[2];
+				saturation = (float) (saturation * (degree / 100));
+				int rgb = Color.HSBtoRGB(hue, saturation, brightness);
+				r = (rgb>>16)&0xFF;
+				g = (rgb>>8)&0xFF;
+				b = rgb&0xFF;
+				Color newColor = new Color(r, g, b);
+				picture.setRGB(j, i, newColor.getRGB());
+			}
+		}
 	}
 
 	/**
@@ -72,6 +101,18 @@ public class Image {
 	 */
 	public void blackNWhite() {
 
+	}
+	
+	/**
+	 * Mirror the image
+	 */
+	public void mirror() {
+		int w = picture.getWidth();
+		int h = picture.getHeight();
+		BufferedImage temp = new BufferedImage(w, h, picture.getType());
+		AffineTransform affineTransform = new AffineTransform(-1,0,0,1,w,0);
+		AffineTransformOp affineTransformOp = new AffineTransformOp(affineTransform, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+		picture = affineTransformOp.filter(picture, temp);
 	}
 
 	public BufferedImage getPicture() {
