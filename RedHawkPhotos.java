eb7420bcaa3b6690d6d4fe5b066885908ebf13ab
jbonsu13 <72279494+jbonsu13@users.noreply.github.com>
2020-11-16 20:34:31 -0500

    Refactored for serialization
    Added userSetUp, writeOut, and writeIn; users can now log in, create accounts, and have data saved to each user.

--- RedHawkPhotos.java
+++ RedHawkPhotos.java
@@ -1,9 +1,11 @@
-
-
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.io.*;
+import java.util.ArrayList;
 
 import javax.swing.*;
 
@@ -13,9 +15,12 @@ import javax.swing.*;
  *  @since 10-14-2020
  */
 public class RedHawkPhotos {
+	
+	static ArrayList<User> users = new ArrayList<User>();
+	
 	public static void main (String[] args) {
-		User a = new User("alex", "1234");
-		
+		//User a = new User("alex", "1234");
+		User a = userSetUp();
 		JFrame logPanel = new JFrame();
 		logPanel.setSize(400, 200);
 		logPanel.setResizable(false);
@@ -94,4 +99,76 @@ public class RedHawkPhotos {
 		logPanel.add(logpanel);
 		logPanel.setVisible(true);
 	}
+	
+	
+	public static User userSetUp() {
+		JLabel message = new JLabel("Are you a retuning user?");
+		int yesorno = JOptionPane.showConfirmDialog(new JFrame(), message, "User setup", JOptionPane.YES_NO_OPTION);
+		if (yesorno == JOptionPane.YES_OPTION) {
+			JList<User> userList = new JList<User>();
+			DefaultListModel<User> usermodel = new DefaultListModel<>();
+			for(User elt : users) {
+				usermodel.addElement(elt);
+			}
+			userList.setModel(usermodel);
+			JScrollPane userpane = new JScrollPane(userList, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+					JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+			userpane.getHorizontalScrollBar().setPreferredSize(new Dimension(0, 13));
+			userpane.getVerticalScrollBar().setPreferredSize(new Dimension(13, 0));
+			
+			int confirm = JOptionPane.showConfirmDialog(new JFrame(), userpane, "Select User", JOptionPane.OK_CANCEL_OPTION);
+			if(confirm == JOptionPane.OK_OPTION) {
+				return userList.getSelectedValue();
+			}
+			return null;
+		}
+		else {
+			JPanel userOptions = new JPanel();
+			userOptions.setLayout(new GridLayout(0, 2, 3, 3));
+			
+			JLabel ausername = new JLabel("Username: ");
+			JLabel apassword = new JLabel("Password: ");
+			JTextField uname = new JTextField(10);
+			JTextField pword = new JTextField(10);
+			
+			userOptions.add(ausername);
+			userOptions.add(uname);
+			userOptions.add(apassword);
+			userOptions.add(pword);
+			
+			int confirm = JOptionPane.showConfirmDialog(new JFrame(), userOptions, "Input Credentials", JOptionPane.OK_CANCEL_OPTION);
+			if(confirm == JOptionPane.OK_OPTION) {
+				User a = new User(uname.getText(), pword.getText());
+				users.add(a);
+				return a;
+			}
+			return null;
+		}
+	}
+	
+	public static void writeOut() {
+		try {
+			FileOutputStream fout = new FileOutputStream("UserList");
+			ObjectOutputStream oout = new ObjectOutputStream(fout);
+			oout.writeObject(users);
+			fout.close();
+			oout.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static void writeIn() {
+		try {
+			FileInputStream fin = new FileInputStream("UserList");
+			ObjectInputStream oin = new ObjectInputStream(fin);
+			users = (ArrayList<User>)oin.readObject();
+			fin.close();
+			oin.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
