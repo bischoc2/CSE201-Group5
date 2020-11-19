8bdbf3558d1d080bf21b4b057efd0b4ab0309286
jbonsu13 <72279494+jbonsu13@users.noreply.github.com>
2020-11-18 21:23:03 -0500

    Updated for serialization, updated documentation
    Added toString method. (when selecting a returning user to log into, displays the name of that users instead of the object reference). 
    Updated documentation for all methods.

--- User.java
+++ User.java
@@ -1,5 +1,6 @@
 import java.io.Serializable;
 
+@SuppressWarnings("serial")
 public class User implements Serializable {
 	
 	/** User class creates users (accounts) with their own image library
@@ -12,18 +13,39 @@ public class User implements Serializable {
 	private String password;
 	private ImageLibrary imagelibrary;
 	
+	/** Basic constructor that creates a new User
+	 * 
+	 * @param u users Username
+	 * @param p users Password
+	 */
 	public User(String u, String p) {
 		// TODO Auto-generated constructor stub
 		userName = u;
 		password = p;
 		imagelibrary = new ImageLibrary();
 	}
+	
+	/** Allows the user to log into their account
+	 * 
+	 * @param user users Username
+	 * @param pass users Password
+	 * @return <p> true if credentials are correct; user matches the instance variable userName and pass equals </p>
+	 * the instance variable password. False if otherwise
+	 */
 	public boolean login(String user, String pass) {
 		if(user.equals(this.userName) && pass.equals(this.password)) return true;
 		return false;
 	}
 	
+	/** Return the ImageLibrary for this user
+	 * 
+	 * @return ImageLibrary object stored in the instance variable imageLibrary
+	 */
 	public ImageLibrary getImageLibrary() {
 		return imagelibrary;
+	}
+	
+	public String toString(){
+		return userName;
 	}
 }
